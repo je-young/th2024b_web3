37bbe73e9bdac78ba322bbbffffe70b15e0f02f0
je-young <jey2965@gmail.com>
2025-04-08 14:32:05 +0900

    JPA day04 todo / @CrossOrigin("*") : 플러터 dio(web)

--- src/main/java/example/day04/todo/controller/TodoController.java
+++ src/main/java/example/day04/todo/controller/TodoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/day04/todos")
 @RequiredArgsConstructor
+@CrossOrigin("*") // 플러터 dio (web)
 public class TodoController {
 
     private final TodoService todoService;
