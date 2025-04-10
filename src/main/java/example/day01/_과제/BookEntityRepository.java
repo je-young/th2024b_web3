678a9387ebf511b692d86a882077058097334605
je-young <jey2965@gmail.com>
2025-04-10 17:13:16 +0900

    Manage-Supplies 새브랜치 추가

--- src/main/java/example/day01/_과제/BookEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package example.day01._과제;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository // @Repository : 데이터베이스 연동을 위한 인터페이스
-public interface BookEntityRepository extends JpaRepository<BookEntity, Long> {
-} // end interface
