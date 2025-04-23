7e9a29bdbabe10d1a697e83ca6def251178b0cdc
je-young <jey2965@gmail.com>
2025-04-23 17:19:44 +0900

    오류상태...

--- src/main/java/web/AppStart.java
+++ src/main/java/web/AppStart.java
@@ -3,7 +3,6 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
